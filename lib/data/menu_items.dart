5bceec6a87b3fe2e7140e19f153661c8506666da
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-02-10 11:40:04 +0200

    Added explainable comments to sections file

--- lib/data/menu_items.dart
+++ lib/data/menu_items.dart
@@ -2,25 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:popup_menu_button/model/menu_item.dart';
 
 class MenuItems{
+  // first group for menu options, Settings and Share
   static const List<MenuItem> itemsFirst = [
     itemSettings,
     itemShare,
     ];
 
+  // 2nd group of menu options for only SignOut option
   static const List<MenuItem> itemsSecond = [
     itemSignOut,
     ];
 
+  // defining menu item for Settings
   static const itemSettings = MenuItem(
       text: 'Settings',
       icon: Icons.settings,
   );
 
+  // menu item for Share
   static const itemShare = MenuItem(
     text: 'Share',
-    icon: Icons.logout,
+    icon: Icons.share,
   );
 
+  // menu item for Logout
   static const itemSignOut = MenuItem(
     text: 'Sign out',
     icon: Icons.logout,
