c05da3c2ac7e549f213b996942bcf8f615cc8de4
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-02-10 11:32:14 +0200

    added meaningful comments to the model

--- lib/model/menu_item.dart
+++ lib/model/menu_item.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+// App's model to be used when creating popup menu button
 
 class MenuItem {
+  //consist of Texts and icons
   final String text;
   final IconData icon;
 
+  // constructor
   const MenuItem({
     required this.text,
     required this.icon,
